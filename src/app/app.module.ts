bd398c2d38c47803d337fd18fb6533a7532072aa
unknown <dhwanij2@gmail.com>
2024-04-12 15:40:18 +0530

    FormArray with 1 textbox working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PraFormsComponent } from './pra-forms/pra-forms.component';
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { PraFormsComponent } from './pra-forms/pra-forms.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pra-forms/pra-forms.component.ts
+++ src/app/pra-forms/pra-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-pra-forms',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrl: './pra-forms.component.scss'
 })
 export class PraFormsComponent {
+
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      userName: ['', Validators.required],
+      userEmail: ['', Validators.required],
+      // -- nested group
+      address: this.fb.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        zip: [''],
+      }),
+      aliases: this.fb.array([this.fb.control('')]),
+    });
+  }
+
+  get aliases() {
+    return this.myForm.get('aliases') as FormArray;
+  }
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
+  }
+
+
+  get formVal() {
+    return this.myForm.value;
+  }
+
+  onSubmit() {
+    console.log('onSubmit called');
+    console.log(this.myForm.value);
+  }
+
+
 
 }
