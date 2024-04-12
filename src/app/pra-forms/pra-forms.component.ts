2c63ffbe17b9fbe5e71f68c1128b224bee9bc384
unknown <dhwanij2@gmail.com>
2024-04-12 16:58:59 +0530

    FormArray with checbox working

--- src/app/pra-forms/pra-forms.component.ts
+++ src/app/pra-forms/pra-forms.component.ts
@@ -24,37 +24,35 @@ export class PraFormsComponent {
         zip: [''],
       }),
       aliases: this.fb.array([this.fb.control('')]),
-      items: this.fb.array([this.createItem()],Validators.required)
+      items: this.fb.array([this.createItem()],Validators.required),
+      hobbies: this.fb.array([],Validators.required),
     });
+
+    this.createHobby('Surfing');
+    this.createHobby('Hiking');
+    this.createHobby('Exploring');
   }
 
-  get aliases() {
-    return this.myForm.get('aliases') as FormArray;
+  createHobby(hobbyName: string) {
+    const hobby = this.fb.group({
+      selected:[null,Validators.required],
+      name:[hobbyName,Validators.required]
+    });
+    this.hobbies.push(hobby);
   }
-  addAlias() {
-    this.aliases.push(this.fb.control(''));
-  }
+  get hobbies() { return this.myForm.get('hobbies') as FormArray; }
 
+  get aliases() { return this.myForm.get('aliases') as FormArray; }
+  addAlias() { this.aliases.push(this.fb.control('')); }
 
-
-  addItem() {
-    // const item = this.fb.group({
-    //   name: ['NNN', Validators.required],
-    //   quantity: [5, [Validators.required, Validators.min(1)]],
-    //   // Add more form controls as needed
-    // });
-
-    this.items.push(this.createItem());
-  }
+  addItem() { this.items.push(this.createItem()); }
   createItem():FormGroup{
     return this.fb.group({
       name:["",Validators.required],
       quantity:["",Validators.required]
     })
   }
-  get items() {
-    return this.myForm.get('items') as FormArray;
-  }
+  get items() { return this.myForm.get('items') as FormArray; }
 
 
   get formVal() {
